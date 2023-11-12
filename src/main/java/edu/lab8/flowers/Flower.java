566448bcdcadded76985e856744e34c102831a7f
loginyuk <lohin.pn@ucu.edu.ua>
2023-11-12 04:40:48 +0200

    minor changes

--- src/main/java/edu/lab8/flowers/Flower.java
+++ src/main/java/edu/lab8/flowers/Flower.java
@@ -3,7 +3,6 @@ package edu.lab8.flowers;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,22 +13,17 @@ import lombok.Getter;
 @Table(name = "flower")
 public class Flower {
     @Id
-    @SequenceGenerator(
-        name = "flower_sequence",
-        sequenceName = "flower_sequence",
-        allocationSize = 1
-    )
 
     private Integer id;
+    private double length;
     private FlowerColor color;
     private double price;
-    private double length;
 
     public Flower() {
     }
 
     // Keep only one of the constructors
-    public Flower(FlowerColor color, double price, double length) {
+    public Flower(double length, FlowerColor color, double price) {
         this.color = color;
         this.price = price;
         this.length = length;
--- src/main/java/edu/lab8/flowers/FlowerService.java
+++ src/main/java/edu/lab8/flowers/FlowerService.java
@@ -6,12 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import lombok.Delegate;
 
 @Service
 public class FlowerService {
