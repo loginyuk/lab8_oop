81f764efa330bed7a308ce593388eb3e49d1fa76
loginyuk <lohin.pn@ucu.edu.ua>
2023-12-21 17:47:30 +0200

    minnor changes

--- src/test/java/edu/lab8/Lab8ApplicationTests.java
+++ src/test/java/edu/lab8/Lab8ApplicationTests.java
@@ -3,7 +3,7 @@ package edu.lab8;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Lab8Application.class)
 class Lab8ApplicationTests {
 
 	@Test
